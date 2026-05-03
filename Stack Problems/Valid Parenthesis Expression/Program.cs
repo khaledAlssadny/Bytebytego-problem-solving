2f108edc5fa32aaa6c016df4d6d351e04481747d
‘khaledAlssadny’ <khaledgmal517@gmail.com~>
2026-05-03 03:04:10 +0300

    Valid Parenthesis Expression-Stack problem 1

---
+++ Stack Problems/Valid Parenthesis Expression/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Valid_Parenthesis_Expression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "()[]{}";
+            Console.WriteLine(ValidParenthesisExpression(input));
+        }
+
+        static bool ValidParenthesisExpression(string s)
+        {
+            Dictionary<char, char> pairs = new Dictionary<char, char>
+            {
+                { '(', ')' },
+                { '{', '}' },
+                { '[', ']' }
+            };
+
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (pairs.ContainsKey(c))
+                {
+                    stack.Push(c);
+                }
+                else if (pairs.ContainsValue(c))
+                {
+                    if (stack.Count == 0 || pairs[stack.Pop()] != c)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
